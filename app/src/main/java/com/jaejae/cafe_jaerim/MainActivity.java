c07f9c265a62a5be9f8eb17c9892de94466a3190
KRavie403 <83668266+KRavie403@users.noreply.github.com>
2022-06-15 10:47:58 +0900

    first

---
+++ app/src/main/java/com/jaejae/cafe_jaerim/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jaejae.cafe_jaerim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
