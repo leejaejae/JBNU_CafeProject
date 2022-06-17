215d34d885c9c99583f1903ebb43bcedc65eedbe
leejaejae <leesengjie@naver.com>
2022-06-17 22:29:22 +0900

    검색 기능 추가

--- app/src/main/java/com/jaejae/cafe_jaerim/SearchList.java
+++ app/src/main/java/com/jaejae/cafe_jaerim/SearchList.java
@@ -1,28 +1,125 @@
 package com.jaejae.cafe_jaerim;
 
-import android.os.Bundle;
-import android.util.Log;
-
+import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchList extends AppCompatActivity {
-    FirebaseDatabase database;
-    DatabaseReference stateRef;
 
-    final String TAG = "TAG+Caferagment";
+    private List<String> list;          // 데이터를 넣은 리스트변수
+    private ListView listView;          // 검색을 보여줄 리스트변수
+    private EditText editSearch;        // 검색어를 입력할 Input 창
+    private searchlistAdapter adapter;      // 리스트뷰에 연결할 아답터
+    private ArrayList<String> arraylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchlist);
 
-        database = FirebaseDatabase.getInstance("https://cafe-jaerim-default-rtdb.firebaseio.com/");
-        stateRef = database.getReference("cafe");  // real time
+        editSearch = findViewById(R.id.editSearch);
+        listView = findViewById(R.id.listView);
 
-        Log.d(TAG, "show cafe search list");
+        // 리스트를 생성한다.
+        list = new ArrayList<>();
+
+        // 검색에 사용할 데이터을 미리 저장한다.
+        settingList();
+
+        // 리스트의 모든 데이터를 arraylist에 복사한다.// list 복사본을 만든다.
+        arraylist = new ArrayList<>();
+        arraylist.addAll(list);
+
+        // 리스트에 연동될 아답터를 생성한다.
+        adapter = new searchlistAdapter(list, this);
+
+        // 리스트뷰에 아답터를 연결한다.
+        listView.setAdapter(adapter);
+
+        // input창에 검색어를 입력시 "addTextChangedListener" 이벤트 리스너를 정의한다.
+        editSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // input창에 문자를 입력할때마다 호출된다.
+                // search 메소드를 호출한다.
+                String text = editSearch.getText().toString();
+                search(text);
+            }
+        });
+        list.clear();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager inputmethod = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputmethod.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+    }
+
+    // 검색을 수행하는 메소드
+    public void search(String charText) {
+
+        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
+        list.clear();
+
+        // 문자 입력이 없을때는 모든 데이터를 보여준다.
+        if (charText.length() == 0) {
+//              list.addAll(arraylist);
+        }
+        // 문자 입력을 할때..
+        else
+        {
+            // 리스트의 모든 데이터를 검색한다.
+            for(int i = 0; i < arraylist.size() ; i++)
+            {
+                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
+                if (arraylist.get(i).toLowerCase().contains(charText))
+                {
+                    // 검색된 데이터를 리스트에 추가한다.
+                    list.add(arraylist.get(i));
+                }
+            }
+        }
+        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
+        adapter.notifyDataSetChanged();
+    }
+
+    // 검색에 사용될 데이터를 리스트에 추가한다.
+    private void settingList(){
+//        list.add("kimjongwon");
+//        list.add("kimgunhui");
+//        list.add("choims");
+//        list.add("parkyoungje");
+//        list.add("kimdongwoong");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
+//        list.add("parkyoungje");
 
 
     }
