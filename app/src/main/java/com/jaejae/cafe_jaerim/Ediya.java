95afd7ef494392fdac9f3c65711a78fb167d5daa
KRavie403 <83668266+KRavie403@users.noreply.github.com>
2022-06-19 04:02:43 +0900

    upload cafe

--- app/src/main/java/com/jaejae/cafe_jaerim/Ediya.java
+++ app/src/main/java/com/jaejae/cafe_jaerim/Ediya.java
@@ -7,14 +7,20 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+
 
 public class Ediya extends AppCompatActivity {
+
+    DatabaseReference bookmark;
+    
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ediya);
 
         ImageButton back_button = (ImageButton) findViewById(R.id.back_button);
+        ImageButton bookmark = (ImageButton) findViewById(R.id.bookmark_pic1);
 
         Intent intent = getIntent();
 //        String filename = intent.getStringExtra("filename");
---
+++ app/src/main/java/com/jaejae/cafe_jaerim/GoogleMapEdiya.java
@@ -0,0 +1,98 @@
+package com.jaejae.cafe_jaerim;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.app.AlertDialog;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GoogleMapEdiya extends AppCompatActivity implements OnMapReadyCallback {
+
+    private com.google.android.gms.maps.GoogleMap googleMap;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE=101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(com.google.android.gms.maps.GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        //35.84666 127.13373 전북대학교 (시작화면)
+        LatLng latlng = new LatLng(35.84666, 127.13373);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        MarkerOptions marketOptions = new MarkerOptions().position(latlng).title("전북대학교 전주캠퍼스");
+        googleMap.addMarker(marketOptions);
+
+        /*
+        // GPS이용 현재위치 표시
+        // 사용자 GPS 요청권한
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            checkLocationPermissionWithRationale();
+        }*/
+    }
+
+    /*public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+
+    private void checkLocationPermissionWithRationale() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("위치정보")
+                        .setMessage("이 앱을 사용하기 위해서는 위치정보에 접근이 필요합니다. 위치정보 접근을 허용하여 주세요.")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(GoogleMap.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        }).create().show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        googleMap.setMyLocationEnabled(true);
+                    }
+                } else {
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }*/
+}
